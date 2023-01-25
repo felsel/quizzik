89a5bb43c4687e3708bf5b32553fc8737b388ff8
Joelly Malet Ekofo <j.ekomalet@gmail.com>
2023-01-25 00:07:47 -0500

    was able to able to pars Config file for secrets

--- quizzik/pysrc/run.py
+++ quizzik/pysrc/run.py
@@ -1,7 +1,14 @@
+import os
+
 import quizziklib
 
+# os.chdir("../")
+# print(os.listdir())
 sum = quizziklib.sum_as_string(2, 2)
 print(sum)
+
+db_user = quizziklib.connect_to_db()
+print(db_user)
 
 count_dict = quizziklib.word_counting.count_word("hello my name is joelly")
 print(count_dict)
--- quizzik/src/lib.rs
+++ quizzik/src/lib.rs
@@ -1,5 +1,51 @@
 use pyo3::prelude::*;
 use std::collections::HashMap;
+use std::fs::{self, File};
+use std::io::{Error, Read};
+
+use serde::Deserialize;
+use sqlx::postgres::{PgPoolOptions, PgRow};
+use sqlx::Pool;
+use toml::from_str;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    database: Database,
+}
+
+#[derive(Debug, Deserialize)]
+struct Database {
+    DB_USER: String,
+    DB_PASS: String,
+    DB_URL: String,
+}
+
+#[pyfunction]
+fn connect_to_db() -> PyResult<String> {
+    // let mut buf: Vec<u8> = vec![];
+    // let mut file = File::open("Config.toml")?;
+    // file.read_to_end(&mut buf).expect("failed to read file");
+    // let config = String::from_utf8(buf).expect("failed to convert config bytes to string");
+
+    let config =
+        fs::read_to_string("Config.toml").expect("failed to convert config bytes to string");
+    let config: Config = toml::from_str(&config).unwrap_or_else(|e| {
+        println!(
+            "Failed to create Config struct out of config file's content. Error: {}",
+            e.message()
+        );
+        Config {
+            database: Database {
+                DB_USER: "".into(),
+                DB_PASS: "".into(),
+                DB_URL: "".into(),
+            },
+        }
+    });
+    println!("{:?}", config);
+    Ok(config.database.DB_USER)
+    // let pool = PgPoolOptions::max_connections(10).connect(self, format!(BD_URL, DB_USER, DB_PASS));
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -26,5 +72,6 @@ fn quizziklib(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     let word_counting_module = PyModule::new(py, "word_counting")?;
     word_counting_module.add_function(wrap_pyfunction!(count_word, m)?)?;
     m.add_submodule(word_counting_module)?;
+    m.add_function(wrap_pyfunction!(connect_to_db, m)?)?;
     Ok(())
 }
